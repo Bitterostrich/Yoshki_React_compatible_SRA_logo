646ce378f835008268387670f1a7104fd3d764c5
Bitterostrich <107961069+Bitterostrich@users.noreply.github.com>
2023-07-29 21:08:31 +0100

    Create Sra_logo.jsx
    React compatible SRA authorization logo

---
+++ Sra_logo.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const Sralogo = () => {
+  return (
+    <div style={{ maxWidth: '275px', maxHeight: '163px' }}>
+      <div style={{ position: 'relative', paddingBottom: '60%', height: 'auto', overflow: 'hidden' }}>
+        <iframe 
+        title="SRA_Logo"
+        className='sra_logo'
+
+          allowTransparency="true" 
+          src="https://cdn.yoshki.com/iframe/55847r.html" 
+          style={{ border: '0px', margin: '0px', padding: '0px', backgroundColor: 'transparent', top: '0px', left: '0px', width: '100%', height: '100%', position: 'absolute' }}
+        >
+          <html>
+            <head>
+              <link rel="stylesheet" href="https://cdn.yoshki.com/responsive.css" />
+            </head>
+            <body style={{ margin: '0px', padding: '0px' }} data-new-gr-c-s-check-loaded="14.1098.0" data-gr-ext-installed="">
+              <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+              <script type="text/javascript" src="https://cdn.yoshki.com/yoshki-library.js"></script>
+              <div className="yoshkidiv" style={{ position: 'relative', left: '0', top: '0', display: 'inline-block', '*display': 'inline' }}>
+                <a href="https://yoshki.com/validate.aspx?083082065+069110103108105115104066087+068101102097117108116046112110103+104116116112115058047047119119119046106101109101107115111108105099105116111114115046099111046117107047" target="_blank" rel='noreferrer'>
+                  <img 
+                    alt="Click to verify status" 
+                    border="0" 
+                    onError="this.onerror=null;this.src='https://cdn.yoshki.com/SRA/EnglishBW/275/0/error-Default.png';" 
+                    src="https://cdn.yoshki.com/SRA/EnglishBW/275/0/_Default.png" 
+                    className="yoshkiimg badgenopopup" 
+                    style={{ width: 'auto', position: 'absolute', top: '0', left: '0', '-webkit-touch-callout': 'none', border: 'none', cursor: 'pointer'}}
+                  />
+                  <img 
+                    alt="Click to verify status" 
+                    undefined="" 
+                    border="0" 
+                    onError="this.onerror=null;this.src='https://cdn.yoshki.com/SRA/EnglishBW/275/0/error-Default.png';" 
+                    src="https://cdn.yoshki.com/SRA/EnglishBW/275/0/-Default.png?a=121*012801280128@550=50=@=80=@=60124012@=70=401$0=40125012@1230124@5501$012@05501260=6" 
+                    className="yoshkiimg badgenopopup" 
+                    style={{ width: 'auto', backgroundRepeat: 'no-repeat', padding: '0px', 'max-width': 'none', backgroundImage: 'url(https://cdn.yoshki.com/SRA/EnglishBW/275/0/=Default.png?a=121*012801280128@550=50=@=80=@=60124012@=70=401$0=40125012@1230124@5501$012@05501260=6)', position: 'relative', top: '0', left: '0', '-webkit-touch-callout': 'none', border: 'none', cursor: 'pointer'}} 
+                  />
+                  <img 
+                    alt="Click to verify status" 
+                    border="0" 
+                    onError="this.onerror=null;this.src='https://cdn.yoshki.com/SRA/EnglishBW/275/0/error-Default.png';" 
+                    src="https://cdn.yoshki.com/SRA/EnglishBW/275/0/_Default.png" 
+                    className="yoshkiimg badgenopopup" 
+                    style={{ width: 'auto', position: 'absolute', top: '0', left: '0', '-webkit-touch-callout': 'none', border: 'none', cursor: 'pointer'}}
+                  />
+                </a>
+              </div>
+              <a id="yoshkiImageLinkPopup_Link" href=""><span style={{ display: 'none' }}>Close Popup</span>
+                <div onContextMenu="return false;" id="yoshkiImageLinkPopup" style={{ 'z-order': '10000', position: 'absolute', display: 'none', padding: '0px', 'max-width': 'none', backgroundImage: 'url(https://cdn.yoshki.com/images/1px.gif)', width: '271px', height: '269px', border: '0px', backgroundRepeat: 'no-repeat' }}></div>
+              </a>
+              <div onContextMenu="return false;" id="yoshkiCopyrightNotice" style={{ 'z-order': '10000', position: 'absolute', display: 'none', backgroundRepeat: 'no-repeat', padding: '0px', 'max-width': 'none', backgroundImage: 'url(https://cdn.yoshki.com/images/1px.gif)', width: '271px', height: '269px', border: '0px'}}></div>
+              <a id="yoshkiCopyrightLinkPopup_Link" href=""><span style={{ display: 'none' }}>Close Popup</span>
+                <div onContextMenu="return false;" id="yoshkiCopyrightNoticeWithCustomLink" style={{ 'z-order': '10000', position: 'absolute', display: 'none', backgroundRepeat: 'no-repeat', padding: '0px', 'max-width': 'none', backgroundImage: 'url(https://cdn.yoshki.com/images/1px.gif)', width: '271px', height: '269px', border: '0px' }}></div>
+              </a>
+              <div style={{ left: '0px', top: '0px', display: 'none' }} id="yoshkiZoom">Loading...</div>
+            </body>
+            <grammarly-desktop-integration data-grammarly-shadow-root="true" />
+          </html>
+        </iframe>
+      </div>
+    </div>
+  );
+};
+
+export default Sralogo;
